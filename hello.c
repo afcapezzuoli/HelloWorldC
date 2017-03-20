343224fe7a933f38a6b58bb52e97167228d44ed7
afcapezzuoli <capezzuoli.agustin@gmail.com>
2017-03-20 00:48:43 -0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,14 @@
+/*TP0 - Hello World
+* Probar el funcionamiento del compilador ejecutando el programa Hello World
+* Agustin Fausto Capezzuoli 1465107
+* 2017-03-20
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	printf("Hello World!");
+	
+	return 0;
+}
